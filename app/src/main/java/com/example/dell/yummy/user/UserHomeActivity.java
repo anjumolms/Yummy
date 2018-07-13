38fdf0baf81dd0a5af3247ceef5a4e48bb059e60
anjumolms <anusha2k1@gmail.com>
2018-07-13 08:20:47 +0530

    test

--- app/src/main/java/com/example/dell/yummy/user/UserHomeActivity.java
+++ app/src/main/java/com/example/dell/yummy/user/UserHomeActivity.java
@@ -124,17 +124,11 @@ public class UserHomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_coin) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_wallet) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_logout) {
 
         }
 
