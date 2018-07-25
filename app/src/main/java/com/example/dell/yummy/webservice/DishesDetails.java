417201235597186efb3b5e78585e3b3518a06606
anjumolms <anusha2k1@gmail.com>
2018-07-25 08:55:35 +0530

    21 july

---
+++ app/src/main/java/com/example/dell/yummy/webservice/DishesDetails.java
@@ -0,0 +1,92 @@
+package com.example.dell.yummy.webservice;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class DishesDetails {
+
+        @SerializedName("item_name")
+        @Expose
+        private String itemName;
+        @SerializedName("item_price")
+        @Expose
+        private int itemPrice;
+        @SerializedName("item_signature")
+        @Expose
+        private int itemSignature;
+        @SerializedName("item_sold_count")
+        @Expose
+        private int itemSoldCount;
+        @SerializedName("item_stock")
+        @Expose
+        private int itemStock;
+        @SerializedName("menu_id")
+        @Expose
+        private int menuId;
+        @SerializedName("retail_id")
+        @Expose
+        private int retailId;
+
+        private int counter = 0;
+
+    public int getCounter() { return counter; }
+
+    public void setCounter(int counter) { this.counter = counter; }
+
+    public String getItemName() {
+            return itemName;
+        }
+
+        public void setItemName(String itemName) {
+            this.itemName = itemName;
+        }
+
+        public int getItemPrice() {
+            return itemPrice;
+        }
+
+        public void setItemPrice(int itemPrice) {
+            this.itemPrice = itemPrice;
+        }
+
+        public int getItemSignature() {
+            return itemSignature;
+        }
+
+        public void setItemSignature(int itemSignature) {
+            this.itemSignature = itemSignature;
+        }
+
+        public int getItemSoldCount() {
+            return itemSoldCount;
+        }
+
+        public void setItemSoldCount(int itemSoldCount) {
+            this.itemSoldCount = itemSoldCount;
+        }
+
+        public int getItemStock() {
+            return itemStock;
+        }
+
+        public void setItemStock(int itemStock) {
+            this.itemStock = itemStock;
+        }
+
+        public int getMenuId() {
+            return menuId;
+        }
+
+        public void setMenuId(int menuId) {
+            this.menuId = menuId;
+        }
+
+        public int getRetailId() {
+            return retailId;
+        }
+
+        public void setRetailId(int retailId) {
+            this.retailId = retailId;
+        }
+
+    }
