a435b35cc4dc8218b2ce7448f5ca4668f772f86d
anjumolms <anusha2k1@gmail.com>
2018-08-16 07:23:04 +0530

    recent updates

--- app/src/main/java/com/example/dell/yummy/user/reviews/UserAddReviewAdapter.java
+++ app/src/main/java/com/example/dell/yummy/user/reviews/UserAddReviewAdapter.java
@@ -13,34 +13,55 @@ import com.example.dell.yummy.webservice.DishesDetails;
 
 import java.util.List;
 
-public class UserAddReviewAdapter extends RecyclerView.Adapter<UserAddReviewAdapter.ReviewViewHolder> {
+public class UserAddReviewAdapter
+        extends RecyclerView.Adapter<UserAddReviewAdapter.ReviewViewHolder> {
 
     private Context mCtx;
-    private List<DishesDetails> dishesDetailsList;
-    TextView textView;
 
-    public UserAddReviewAdapter(Context mCtx, List<DishesDetails> dishesDetailsList) {
+    private List<UserReview> userReviews;
+
+
+    public UserAddReviewAdapter(Context mCtx, List<UserReview> userReviews) {
         this.mCtx = mCtx;
-        this.dishesDetailsList = dishesDetailsList;
+        this.userReviews = userReviews;
     }
 
     @Override
-    public ConfirmationAdapter.ReviewViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public UserAddReviewAdapter.ReviewViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         //inflating and returning our view holder
         LayoutInflater inflater = LayoutInflater.from(mCtx);
-        View view = inflater.inflate(R.layout.add_review, parent,false);
-        return new ConfirmationAdapter.ReviewViewHolder(view);
+        View view = inflater.inflate(R.layout.add_review, parent, false);
+        return new ReviewViewHolder(view);
     }
+
     @Override
-    public void onBindViewHolder(ConfirmationAdapter.ReviewViewHolder holder, int position) {
+    public void onBindViewHolder(UserAddReviewAdapter.ReviewViewHolder holder, int position) {
         //getting the product of the specified position
 
 
+        //binding the data with the viewholder views
+        holder.add_review_itemname.setText(userReviews.get(position).getOrderItem());
 
-            //binding the data with the viewholder views
-            holder.textView.setText(dishesDetails.getItemName());
-
-        }
 
     }
 
+    @Override
+    public int getItemCount() {
+        return userReviews.size();
+    }
+
+    class ReviewViewHolder extends RecyclerView.ViewHolder {
+
+        TextView add_review_itemname, textViewRating;
+
+
+        public ReviewViewHolder(View itemView) {
+            super(itemView);
+
+            add_review_itemname = itemView.findViewById(R.id.add_review_itemname);
+
+            textViewRating = itemView.findViewById(R.id.add_rating);
+
+        }
+    }
+}
